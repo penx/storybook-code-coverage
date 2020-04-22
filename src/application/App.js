b0943269d75ef28151311021da87b95820321ad4
Alasdair McLeay <alasdair.mcleay@me.com>
2020-04-22 23:26:21 +0100

    add examples of unreached code in application

--- src/application/App.js
+++ src/application/App.js
@@ -3,8 +3,9 @@ import { useToggle } from "../utilities/useToggle";
 import { Toggle } from "../design-system/Toggle";
 import { GlobalStyles } from "../design-system/GlobalStyles";
 import { Switch, Route, Link } from "react-router-dom";
-import { About } from './About';
-import { Users } from './Users';
+import { About } from "./About";
+
+const Users = React.lazy(() => import('./Users'));
 
 function App() {
   const { on: darkModeOn, toggle: toggleDarkMode } = useToggle();
@@ -24,16 +25,40 @@ function App() {
             <li>
               <Link to="/users">Users</Link>
             </li>
+            {darkModeOn && (
+              <li>
+                <Link to="/secret">Hidden Link</Link>
+              </li>
+            )}
           </ul>
         </nav>
         <Switch>
-          <Route path="/about"><About /></Route>
-          <Route path="/users"><Users /></Route>
-          <Route path="/">Home</Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <h1>Home</h1>
+            <p>Home page</p>
+          </Route>
+          {darkModeOn && (
+            <Route path="/secret">
+              <DarkModePage />
+            </Route>
+          )}
         </Switch>
       </div>
     </GlobalStyles>
   );
 }
+
+const DarkModePage = () => (
+  <>
+    <h1>Hidden Page</h1>
+    <p>This route is only available in dark mode</p>
+  </>
+);
 
 export default App;
