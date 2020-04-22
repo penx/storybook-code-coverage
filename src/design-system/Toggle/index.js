3ce0deeb533c6f80443f37f29d604005d8128296
Alasdair McLeay <alasdair.mcleay@me.com>
2020-04-22 23:25:54 +0100

    make toggle button text white by default

--- src/design-system/Toggle/index.js
+++ src/design-system/Toggle/index.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 const StyledButton = styled.button`
   background-color: ${(props) => props.theme.primary || "green"};
-  color: ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text || "white"};
 `;
 
 export const Toggle = ({ on, onToggle }) => (
